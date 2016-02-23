629cbe1721f1599d9314252c5a782d5f0f3c6bd4
Richard Cheng <rlcheng.7@gmail.com>
2016-02-22 23:06:42 -0800

    Associate User model with Visit model

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,27 +1,32 @@
 class VisitsController < ApplicationController
+  before_action :find_user
   before_action :find_visit, only: [:destroy]
   before_action :authorize
 
   def create
-    count = Visit.count
-    @visit = Visit.where(name: visit_params[:name])
+    count = @user.visits.count
+    @visit = @user.visits.where(name: visit_params[:name])
       .first_or_create(visit_params)
-    @visit.update(visit_params) if Visit.count == count
-    redirect_to visits_path
+    @visit.update(visit_params) if @user.visits.count == count
+    redirect_to user_visits_path
   end
 
   def index
-    @visits = Visit.order(:time)
+    @visits = @user.visits.order(:time)
   end
 
   def destroy
     @visit.destroy
-    redirect_to visits_path
+    redirect_to user_visits_path
   end 
 
   private
+    def find_user
+      @user = User.find(params[:user_id])
+    end
+
     def find_visit
-      @visit = Visit.find(params[:id])
+      @visit = @user.visits.find(params[:id])
     end
 
     def visit_params
